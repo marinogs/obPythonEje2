d7ef8968b9fea45e53941457e0de1e2dd44430c7
Marino Guadia Suárez <marinoguardiasuarez@gmail.com>
2022-02-15 18:28:43 +0100

    Primer commit Ejercicio 2

---
+++ main.py
@@ -0,0 +1,15 @@
+# OpenBootcamp Curso Python Ejercicio 2
+
+# En este ejercicio tendrás que crear un archivo python donde tendrás que crear una variable a la cual asignarás una
+# cadena de texto y la imprimirás por consola.
+#
+# Por otro lado, tendrás que modificar la variable y después imprimirla por consola para ver la modificación
+# de la variable.
+
+cadena = "Hola mundo"
+
+print(cadena)
+
+cadena = "Hello World"
+
+print(cadena)
